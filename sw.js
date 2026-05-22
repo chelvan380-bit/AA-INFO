bbad91daa17e193d6ba2504d97a30978e77b594b
chelvan380-bit <chelvan380@gmail.com>
2026-05-22 23:29:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,33 @@
-// AA & PADLOCK INFO - Service Worker v2.0
-const CACHE_NAME = 'aa-padlock-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+// sw.js
+
+const CACHE_NAME = "aa-info-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
+self.addEventListener("fetch", event => {
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
